d6dc0df982153d8de0e797a6475303860b5b5109
lenny <1941973283@qq.com>
2018-04-13 10:46:48 +0800

    zuul 自定义过滤器

--- service-gateway/src/main/java/com/smallcode/springcloudsample/gateway/GatewayApplication.java
+++ service-gateway/src/main/java/com/smallcode/springcloudsample/gateway/GatewayApplication.java
@@ -1,9 +1,11 @@
 package com.smallcode.springcloudsample.gateway;
 
 
+import com.smallcode.springcloudsample.gateway.filter.PreRequestLogFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,4 +14,10 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+
+    @Bean
+    public PreRequestLogFilter preRequestLogFilter() {
+        return new PreRequestLogFilter();
+    }
+
 }
